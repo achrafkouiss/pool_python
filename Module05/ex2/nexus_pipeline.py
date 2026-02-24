826c1fa85efefe757b6847991016ad0c84227ce2
Achraf Kouiss <akouiss@c2r9p12.1337.ma>
2026-02-24 02:31:31 +0000

    skelton for ex2

--- Module05/ex2/nexus_pipeline.py
+++ Module05/ex2/nexus_pipeline.py
@@ -1,5 +1,65 @@
 from abc import ABC, abstractmethod
 from typing import Any, List, Dict, Union, Optional, Protocol
 
+
 class ProcessingPipeline(ABC):
-    pass
+    # stages: List[ProcessingStage] = []
+
+    def add_stage(self):
+        print("xxxxxxxxxxxxx")
+
+    def process(self, data: Any) -> Union[str, Any]:
+        pass
+
+class JSONAdapter(ProcessingPipeline):
+    def __init__(self, pipeline_id):
+        self.pipeline_id = pipeline_id
+
+    
+    def process(slef, data) -> Any:
+        pass
+
+class CSVAdapter(ProcessingPipeline):
+    def __init__(self, pipeline_id):
+        self.pipeline_id = pipeline_id
+
+    def process(slef, data) -> Any:
+        pass
+
+class StreamAdapter(ProcessingPipeline):
+    def __init__(self, pipeline_id):
+        self.pipeline_id = pipeline_id
+
+    def process(slef, data) -> Any:
+        pass
+
+
+class ProcessingStage(Protocol):
+    def process(self, data: Any) -> Any:
+        pass
+
+class InputStage():
+    def process(self, data: Any) -> Dict:
+        pass
+
+class TransformStage():
+    def process(self, data: Any) -> Dict:
+        pass
+
+class OutputStage():
+    def process(self, data: Any) -> str:
+        pass
+
+
+
+
+class NexusManager():
+    def add_pipeline():
+        pass
+
+    def process_data():
+        pass
+
+if __name__ == "__main__":
+    a = ProcessingPipeline()
+    a.add_stage()
