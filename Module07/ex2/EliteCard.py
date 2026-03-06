47162e4c04afa66e33ec6604fe4efdc04bb7d4fd
Achraf <achraf@example.com>
2026-03-06 07:50:18 +0000

    working on ex2

--- Module07/ex2/EliteCard.py
+++ Module07/ex2/EliteCard.py
@@ -1,7 +1,7 @@
 from ex0.Card import Card
 from ex2 import Magical, Combatable
 
-class EliteCard(Card, Magical, Combatable):
+class EliteCard(Card, Combatable, Magical,):
     def cast_spell(self, spell_name: str, targets: list) -> dict:
         pass
 
@@ -11,14 +11,40 @@ class EliteCard(Card, Magical, Combatable):
     def get_magic_stats(self) -> dict:
         pass
 
-    def attack(self, target) -> dict:
-        pass
+    def attack(self, target: Card) -> dict:
+        return {
+            "attacker": self.name,
+            "target": target.name,
+            "damage": self.cost,
+            "combat_type": "melee"
+        }
 
     def defend(self, incoming_damage: int) -> dict:
-        pass
+        alive = max(0, self.cost - incoming_damage)         
+        return {
+            "defender": self.name,
+            "damage_taken": incoming_damage,
+            "damage_blocked": self.cost - incoming_damage,
+            "still_alive": alive >= 0
+        }
 
     def get_combat_stats(self) -> dict:
         pass
 
     def play(self, game_state: dict) -> dict:
-        pass
+        if "mana" not in game_state:
+            return {
+                "error": "game_state should contain mana as a key",
+            }
+        if self.is_playable(game_state["mana"]):
+            return {
+            "card_played": self.name,
+            "mana_used": self.cost,
+            "effect": "EliteCard is played",
+        }
+        return {
+            "card_played": self.name,
+            "mana_used": 0,
+            "effect": "Cannot play EliteCard insufficient mana"
+        }
+    
--- Module07/ex2/main.py
+++ Module07/ex2/main.py
@@ -1,10 +1,21 @@
 from ex0.Card import Card
 from ex2.Combatable import Combatable
 from ex2.Magical import Magical
+from ex2.EliteCard import EliteCard
 
 print("\n=== DataDeck Ability System ===\n")
 
 print("EliteCard capabilities:")
-print("- Card: ", [name for name, attribute in Card.__dict__.items() if callable(attribute) and not name.startswith('__')])
-print("- Combatable: ", [name for name, attribute in Combatable.__dict__.items() if callable(attribute) and not name.startswith('__')])
-print("- Magical: ", [name for name, attribute in Magical.__dict__.items() if callable(attribute) and not name.startswith('__')])
+print("- Card: ",
+       [name for name, attribute in Card.__dict__.items() if callable(attribute) and not name.startswith('__')])
+print("- Combatable: ",
+       [name for name, attribute in Combatable.__dict__.items() if callable(attribute) and not name.startswith('__')])
+print("- Magical: ",
+       [name for name, attribute in Magical.__dict__.items() if callable(attribute) and not name.startswith('__')])
+
+print("\nPlaying Arcane Warrior (Elite Card):\n")
+
+arcane_warrior = EliteCard("Arcane Warrior", 5, "Elite")
+enemy = EliteCard("Enemy", 3, "common")
+print("Combat phase:")
+print(f"Attack result: {arcane_warrior.attack(enemy)}")
